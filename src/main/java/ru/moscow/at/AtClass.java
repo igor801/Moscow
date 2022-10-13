d91a82910b4ede1dbf64e24183e6f3e13ff3ab2c
Igor Babikov <babikov_igor@inbox.ru>
2022-10-13 11:50:18 +0300

    13.10.2022

---
+++ src/main/java/ru/moscow/at/AtClass.java
@@ -0,0 +1,10 @@
+package ru.moscow.at;
+
+/**
+ * @author 19583543
+ */
+public class AtClass {
+    public static void main(String[] args) {
+        System.out.println("more");
+    }
+}
---
+++ src/main/java/ru/moscow/mt/MTClass.java
@@ -0,0 +1,10 @@
+package ru.moscow.mt;
+
+public class MTClass {
+    public static void main(String[] args) {
+        int a = 1200;
+        short b = 12345;
+        String hello = "hello1212";
+        System.out.println(hello);
+    }
+}
