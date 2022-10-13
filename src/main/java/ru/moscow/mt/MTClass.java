877d063a0ebd3a6f816766a9b125e339101b33a0
Igor Babikov <babikov_igor@inbox.ru>
2022-10-13 15:54:36 +0300

    today 3

--- src/main/java/ru/moscow/mt/MTClass.java
+++ src/main/java/ru/moscow/mt/MTClass.java
@@ -2,9 +2,13 @@ package ru.moscow.mt;
 
 public class MTClass {
     public static void main(String[] args) {
-        for (int c = 0; c < 5; c++) {
-        System.out.println(a += 1);
+        int a;
+        int b;
+        a = 23;
+        b = 23;
+        if (a != b) {
+            System.out.println("true");
+        } else System.out.println("false");
 
-        }
     }
 }
